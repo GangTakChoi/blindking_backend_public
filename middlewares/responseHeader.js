6c611f4f395d9565fcb89e03909597ceb420c8eb
ChoiKT <chlrkdxkr@gmail.com>
2023-08-31 02:41:10 +0900

    fix cors error

--- middlewares/responseHeader.js
+++ middlewares/responseHeader.js
@@ -1,6 +1,5 @@
 exports.setHeader = (req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", `${process.env.ORIGIN_URL}`)
-  res.header("Access-Control-Allow-Origin", `http://localhost:8080`);
+  res.header("Access-Control-Allow-Origin", `${process.env.ORIGIN_URL}`);
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type,Content-Security-Policy,Authorization"
